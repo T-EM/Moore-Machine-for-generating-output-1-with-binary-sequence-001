506cc763825c57bd6c0cf5efb6f21b25887414e4
T-EM <95489786+T-EM@users.noreply.github.com>
2023-04-22 02:11:16 +0530

    Delete Web directory

--- Web/bin/trand.js
+++
@@ -1,9 +0,0 @@
-// import { LeaderLine } from "./leader-line.min.js";
-
-let q1 = document.getElementById("q1");
-let q5 = document.getElementById("q5");
-
-// new LeaderLine(q1, q5);
-// new LeaderLine(q1, q5, { color: "red", size: 8 });
-// new LeaderLine(document.getElementById("q1"), document.getElementById("q5"));
-var line = new LeaderLine(q1, q2, { hide: true });
