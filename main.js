1c38a9036ab8855371c6d785bb04f9ca11217e6d
T-EM <95489786+T-EM@users.noreply.github.com>
2023-04-22 17:42:07 +0530

    mobile devices adjusment (Responsive)

--- main.js
+++ main.js
@@ -5,23 +5,54 @@ let solState = document.getElementById("solstate");
 
 let isLine = false;
 
+content.addEventListener(
+  "scroll",
+  AnimEvent.add(function () {
+    q12.position();
+    q15.position();
+    q23.position();
+    q25.position();
+    q34.position();
+    q35.position();
+    q42.position();
+    q45.position();
+    q52.position();
+    q55.position();
+  }),
+  false
+);
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
 function home() {
   if (isLine === true) {
     toogle(0);
     setTimeout(() => {
       probState.style.transform = "translateY(-150px)";
-      lgState.style.transform = "translateY(100px)";
+      lgState.style.top = "50%";
+      lgState.style.transform = "translateY(0)";
       lgState.style.fontSize = "90px";
       solState.style.transform = "translateY(600px)";
       content.style.overflowY = "hidden";
+      probState.style.visibility = "hidden";
+      solState.style.visibility = "hidden";
     }, 500);
     isLine = false;
   } else {
     probState.style.transform = "translateY(-150px)";
-    lgState.style.transform = "translateY(100px)";
+    lgState.style.top = "50%";
+    lgState.style.transform = "translateY(0)";
     lgState.style.fontSize = "90px";
     solState.style.transform = "translateY(600px)";
     content.style.overflowY = "hidden";
+    probState.style.visibility = "hidden";
+    solState.style.visibility = "hidden";
   }
 }
 
@@ -29,7 +60,9 @@ function proClick() {
   if (isLine === true) {
     home();
     setTimeout(() => {
+      probState.style.visibility = "visible";
       probState.style.transform = "translateY(200px)";
+      lgState.style.top = "0";
       lgState.style.transform = "translateY(380px)";
       lgState.style.fontSize = "50px";
       solState.style.transform = "translateY(600px)";
@@ -37,7 +70,9 @@ function proClick() {
     }, 600);
   } else {
     home();
+    probState.style.visibility = "visible";
     probState.style.transform = "translateY(200px)";
+    lgState.style.top = "0";
     lgState.style.transform = "translateY(380px)";
     lgState.style.fontSize = "50px";
     solState.style.transform = "translateY(600px)";
@@ -49,10 +84,12 @@ function solClick() {
   if (isLine === true) {
     home();
     setTimeout(() => {
+      solState.style.visibility = "visible";
+      lgState.style.top = "0";
       lgState.style.transform = "translateY(-150px)";
       solState.style.transform = "translateY(-100px)";
       lgState.style.fontSize = "70px";
-      content.style.overflowY = null;
+      content.style.overflowY = "scroll";
       setTimeout(() => {
         toogle(1);
         isLine = true;
@@ -60,10 +97,12 @@ function solClick() {
     }, 600);
   } else {
     home();
+    solState.style.visibility = "visible";
+    lgState.style.top = "0";
     lgState.style.transform = "translateY(-150px)";
     solState.style.transform = "translateY(-100px)";
     lgState.style.fontSize = "70px";
-    content.style.overflowY = null;
+    content.style.overflowY = "scroll";
     setTimeout(() => {
       toogle(1);
       isLine = true;
