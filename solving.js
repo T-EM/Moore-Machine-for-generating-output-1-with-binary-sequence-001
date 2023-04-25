fbb3c3b08f008d11a18863510bc08c0894696646
T-EM <95489786+T-EM@users.noreply.github.com>
2023-04-25 23:42:41 +0530

    Added-Transition-Table-Animations

--- solving.js
+++ solving.js
@@ -1,3 +1,4 @@
+// Transition Diagrams Starts
 let q1 = document.getElementById("q1");
 let q2 = document.getElementById("q2");
 let q3 = document.getElementById("q3");
@@ -16,6 +17,26 @@ let q42;
 let q45;
 let q52;
 let q55;
+// Ends
+
+// Transition Table Starts
+let qt1 = document.getElementById("qt1");
+let qt2 = document.getElementById("qt2");
+let qt3 = document.getElementById("qt3");
+let qt4 = document.getElementById("qt4");
+let qt5 = document.getElementById("qt5");
+
+let qt12 = document.getElementById("qt12");
+let qt15 = document.getElementById("qt15");
+let qt23 = document.getElementById("qt23");
+let qt25 = document.getElementById("qt25");
+let qt35 = document.getElementById("qt35");
+let qt34 = document.getElementById("qt34");
+let qt42 = document.getElementById("qt42");
+let qt45 = document.getElementById("qt45");
+let qt52 = document.getElementById("qt52");
+let qt55 = document.getElementById("qt55");
+// Ends
 
 let totalString = "";
 let solvingSpeed = setSpeed.value;
@@ -28,15 +49,24 @@ class MooreMachine {
   }
   q1() {
     q1.style.backgroundColor = "darkmagenta";
+    qt1.style.backgroundColor = "darkmagenta";
     moore_output.innerText = "^";
+    if (this.str[0] == "0") {
+      qt12.style.backgroundColor = "darkmagenta";
+    } else if (this.str[0] == "1") {
+      qt15.style.backgroundColor = "darkmagenta";
+    }
     setTimeout(() => {
       q1.style.backgroundColor = "transparent";
+      qt1.style.backgroundColor = "transparent";
       if (this.str[0] == "0") {
         q12.color = "darkmagenta";
+        qt12.style.backgroundColor = "darkmagenta";
         this.pop();
         this.q2();
       } else if (this.str[0] == "1") {
         q15.color = "darkmagenta";
+        qt15.style.backgroundColor = "darkmagenta";
         this.pop();
         this.q5();
       } else {
@@ -46,20 +76,32 @@ class MooreMachine {
   }
   q2() {
     q2.style.backgroundColor = "darkmagenta";
+    qt2.style.backgroundColor = "darkmagenta";
     // console.log("0");
     totalString += "0";
     moore_output.innerText = totalString;
+    if (this.str[0] == "0") {
+      qt23.style.backgroundColor = "darkmagenta";
+    } else if (this.str[0] == "1") {
+      qt25.style.backgroundColor = "darkmagenta";
+    }
     setTimeout(() => {
+      q2.style.backgroundColor = "transparent";
+      qt2.style.backgroundColor = "transparent";
       q12.color = "#f6f6f6";
       q52.color = "#f6f6f6";
       q42.color = "#f6f6f6";
-      q2.style.backgroundColor = "transparent";
+      qt12.style.backgroundColor = "transparent";
+      qt52.style.backgroundColor = "transparent";
+      qt42.style.backgroundColor = "transparent";
       if (this.str[0] == "0") {
         q23.color = "darkmagenta";
+        qt23.style.backgroundColor = "darkmagenta";
         this.pop();
         this.q3();
       } else if (this.str[0] == "1") {
         q25.color = "darkmagenta";
+        qt25.style.backgroundColor = "darkmagenta";
         this.pop();
         this.q5();
       } else {
@@ -69,18 +111,28 @@ class MooreMachine {
   }
   q3() {
     q3.style.backgroundColor = "darkmagenta";
+    qt3.style.backgroundColor = "darkmagenta";
     //   console.log("0");
     totalString += "0";
     moore_output.innerText = totalString;
+    if (this.str[0] == "0") {
+      qt35.style.backgroundColor = "darkmagenta";
+    } else if (this.str[0] == "1") {
+      qt34.style.backgroundColor = "darkmagenta";
+    }
     setTimeout(() => {
-      q23.color = "#f6f6f6";
       q3.style.backgroundColor = "transparent";
+      qt3.style.backgroundColor = "transparent";
+      q23.color = "#f6f6f6";
+      qt23.style.backgroundColor = "transparent";
       if (this.str[0] == "0") {
         q35.color = "darkmagenta";
+        qt35.style.backgroundColor = "darkmagenta";
         this.pop();
         this.q5();
       } else if (this.str[0] == "1") {
         q34.color = "darkmagenta";
+        qt34.style.backgroundColor = "darkmagenta";
         this.pop();
         this.q4();
       } else {
@@ -90,18 +142,28 @@ class MooreMachine {
   }
   q4() {
     q4.style.backgroundColor = "darkgreen";
+    qt4.style.backgroundColor = "darkgreen";
     //   console.log("1");
     totalString += "1";
     moore_output.innerText = totalString;
+    if (this.str[0] == "0") {
+      qt42.style.backgroundColor = "darkmagenta";
+    } else if (this.str[0] == "1") {
+      qt45.style.backgroundColor = "darkmagenta";
+    }
     setTimeout(() => {
-      q34.color = "#f6f6f6";
       q4.style.backgroundColor = "transparent";
+      qt4.style.backgroundColor = "transparent";
+      q34.color = "#f6f6f6";
+      qt34.style.backgroundColor = "transparent";
       if (this.str[0] == "0") {
         q42.color = "darkmagenta";
+        qt42.style.backgroundColor = "darkmagenta";
         this.pop();
         this.q2();
       } else if (this.str[0] == "1") {
         q45.color = "darkmagenta";
+        qt45.style.backgroundColor = "darkmagenta";
         // q4.style.backgroundColor = "transparent";
         this.pop();
         this.q5();
@@ -112,22 +174,36 @@ class MooreMachine {
   }
   q5() {
     q5.style.backgroundColor = "darkmagenta";
+    qt5.style.backgroundColor = "darkmagenta";
     //   console.log("0");
     totalString += "0";
     moore_output.innerText = totalString;
+    if (this.str[0] == "0") {
+      qt52.style.backgroundColor = "darkmagenta";
+    } else if (this.str[0] == "1") {
+      qt55.style.backgroundColor = "darkmagenta";
+    }
     setTimeout(() => {
       q5.style.backgroundColor = "transparent";
+      qt5.style.backgroundColor = "transparent";
       q15.color = "#f6f6f6";
       q25.color = "#f6f6f6";
       q35.color = "#f6f6f6";
       q45.color = "#f6f6f6";
       q55.color = "#f6f6f6";
+      qt15.style.backgroundColor = "transparent";
+      qt25.style.backgroundColor = "transparent";
+      qt35.style.backgroundColor = "transparent";
+      qt45.style.backgroundColor = "transparent";
+      qt55.style.backgroundColor = "transparent";
       if (this.str[0] == "0") {
         q52.color = "darkmagenta";
+        qt52.style.backgroundColor = "darkmagenta";
         this.pop();
         this.q2();
       } else if (this.str[0] == "1") {
         q55.color = "darkmagenta";
+        qt55.style.backgroundColor = "darkmagenta";
         this.pop();
         this.q5();
       } else {
